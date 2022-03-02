f5714a890df817a8a9d23367aa9135089f0e34c5
jiyuujin <jiyuujinunite@gmail.com>
2022-03-02 14:30:26 +0900

    delete: provider

--- lib/controllers/theme_controller.dart
+++ lib/controllers/theme_controller.dart
@@ -3,8 +3,3 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final themeSelectorProvider =
     StateProvider<ThemeMode>((ref) => ThemeMode.system);
-
-final isDarkModeProvider = Provider<bool>((ref) {
-  final themeMode = ref.watch(themeSelectorProvider.notifier).state;
-  return themeMode == ThemeMode.dark;
-});
